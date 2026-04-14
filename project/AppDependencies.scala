09a6755a0eb4c81f77d2e6a67725dc4c2c925d4d
Okeremeta Jude Tega <31142281+justega247@users.noreply.github.com>
2026-04-14 11:26:21 +0100

    update-pekko: Update Pekko version

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -10,24 +10,24 @@ object AppDependencies {
     "org.typelevel"    %% "cats-core"                       % catsVersion,
     "org.json"          % "json"                            % "20251224",
     "io.lemonlabs"     %% "scala-uri"                       % "4.0.3",
-    "org.apache.pekko" %% "pekko-connectors-xml"            % "1.1.0",
-    "org.apache.pekko" %% "pekko-connectors-json-streaming" % "1.1.0",
-    "org.apache.pekko" %% "pekko-protobuf-v3" % "1.1.3",
-    "org.apache.pekko" %% "pekko-serialization-jackson" % "1.1.3",
-    "org.apache.pekko" %% "pekko-stream" % "1.1.3",
-    "org.apache.pekko" %% "pekko-actor-typed" % "1.1.3"
+    "org.apache.pekko" %% "pekko-connectors-xml"            % "1.3.0",
+    "org.apache.pekko" %% "pekko-connectors-json-streaming" % "1.3.0",
+    "org.apache.pekko" %% "pekko-protobuf-v3" % "1.5.0",
+    "org.apache.pekko" %% "pekko-serialization-jackson" % "1.5.0",
+    "org.apache.pekko" %% "pekko-stream" % "1.5.0",
+    "org.apache.pekko" %% "pekko-actor-typed" % "1.5.0"
   )
 
   val test: Seq[ModuleID] = Seq(
-    "org.mockito"        % "mockito-core"           % "5.15.2",
+    "org.mockito"        % "mockito-core"           % "5.23.0",
     "org.typelevel"     %% "cats-core"              % catsVersion,
-    "org.scalacheck"    %% "scalacheck"             % "1.18.1",
+    "org.scalacheck"    %% "scalacheck"             % "1.19.0",
     "org.typelevel"     %% "discipline-scalatest"   % "2.3.0",
     "uk.gov.hmrc"       %% "bootstrap-test-play-30" % bootstrapPlayVersion,
-    "org.scalacheck"    %% "scalacheck"              % "1.18.1",
-    "org.mockito"        % "mockito-core"            % "5.15.2",
+    "org.scalacheck"    %% "scalacheck"              % "1.19.0",
+    "org.mockito"        % "mockito-core"            % "5.23.0",
     "org.scalatestplus" %% "mockito-5-12"            % "3.2.19.0",
-    "org.scalacheck"    %% "scalacheck"              % "1.18.1",
+    "org.scalacheck"    %% "scalacheck"              % "1.19.0",
     "org.scalatestplus" %% "scalacheck-1-18"         % "3.2.19.0",
 
   ).map(_ % Test)
